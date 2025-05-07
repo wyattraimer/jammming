08ab8c3e42ea70b1b605d0d02611f7eadb3e4386
Wyatt Raimer <wyattraimer@gmail.com>
2025-05-07 15:32:21 -0600

    passing searchresults to tracklist

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from './SearchResults.module.css';
 
-function SearchResults () {
+import Tracklist from '../Tracklist/Tracklist';
+
+function SearchResults (props) {
     return (
         <div className={styles.SearchResults}>
-        {/* <!-- Add a TrackList component --> */}
+        <Tracklist userSearchResults={props.userSearchResults}></Tracklist>
       </div>
         );
 }
