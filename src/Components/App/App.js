e9caaa0b687807ef4c0d2de98c0efb1d2b7f95b7
Wyatt Raimer <wyattraimer@gmail.com>
2025-05-07 10:13:16 -0600

    moved to components folder. fix importing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,19 +4,16 @@ import SearchResults from '../SearchResults/SearchResults.js';
 import Playlist from '../Playlist/Playlist.js';
 import Tracklist from '../Tracklist/Tracklist.js';
 import Track from '../Track/Track.js';
-import Button from '../Button/Button.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <SearchBar></SearchBar>
-        <Button label="Search"></Button>
         <SearchResults></SearchResults>
         <Playlist></Playlist>
         <Tracklist></Tracklist>
         <Track></Track>
-        <Button label='Save To Spotify'></Button>
       </header>
     </div>
   );
