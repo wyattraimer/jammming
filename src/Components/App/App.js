4676daaccec10cd5c71de2bf59b60138172a0e0c
Wyatt Raimer <wyattraimer@gmail.com>
2025-05-07 11:26:57 -0600

    trying to hookup styles?

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,11 @@
-import './App.module.css';
-import SearchBar from '../SearchBar/SearchBar.js';
-import SearchResults from '../SearchResults/SearchResults.js';
-import Playlist from '../Playlist/Playlist.js';
-import Tracklist from '../Tracklist/Tracklist.js';
-import Track from '../Track/Track.js';
+import styles from './App.module.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
         <SearchBar></SearchBar>
-        <SearchResults></SearchResults>
-        <Playlist></Playlist>
-        <Tracklist></Tracklist>
-        <Track></Track>
       </header>
     </div>
   );
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import styles from './SearchBar.module.css'
+import React from 'react';
 
 const SearchBar = () => {
   return (
-    <div>
-      <input type='text' placeholder='Type your song here'/>
+    <div className='SearchBar'>
+      <input type='text' placeholder='Enter A Song, Album, or Artist'/>
     </div>
   )
 }
