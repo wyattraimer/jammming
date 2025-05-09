8ef0b41f921e8fb0c8d4eb8a831028d3767c2656
Wyatt Raimer <wyattraimer@gmail.com>
2025-05-09 09:19:24 -0600

    method will be called when playlist name change happens

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,15 +4,18 @@ import styles from './Playlist.module.css';
 import Tracklilst from '../Tracklist/Tracklist';
 
 function Playlist(props) {
+  function handleNameChange({ target }){
+    props.onNameChange(target.value);
+  }
+
   return (
     <div className="Playlist">
-      <input defaultValue={"New Playlist"} />
+      <input defaultValue={"New Playlist"} onChange={handleNameChange}/>
       <Tracklilst 
         userSearchResults={props.playlistTracks}
         onRemove={props.onRemove}
         isRemoval={true}
-      >
-      </Tracklilst>
+      />
       <button className={styles["Playlist-save"]}>
         SAVE TO SPOTIFY
       </button>
